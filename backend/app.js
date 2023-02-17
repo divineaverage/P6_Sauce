7c8369dcc1f697380ba8e007f1ced09e33e2e9f3
Lindsay J <lindsay.jawor@gmail.com>
2023-02-17 12:14:21 -0500

    set up initial app and server, created route controller and model folders and files

--- backend/app.js
+++ backend/app.js
@@ -1,47 +1,24 @@
-const http = require('http');
-const app = require('./app');
+const express = require('express');
 
-const normalizePort = val => {
-  const port = parseInt(val, 10);
+const app = express();
 
-  if (isNaN(port)) {
-    return val;
-  }
-  if (port >= 0) {
-    return port;
-  }
-  return false;
-};
-const port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
-
-const errorHandler = error => {
-  if (error.syscall !== 'listen') {
-    throw error;
-  }
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
-  switch (error.code) {
-    case 'EACCES':
-      console.error(bind + ' requires elevated privileges.');
-      process.exit(1);
-      break;
-    case 'EADDRINUSE':
-      console.error(bind + ' is already in use.');
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-};
-
-const server = http.createServer(app);
-
-server.on('error', errorHandler);
-server.on('listening', () => {
-  const address = server.address();
-  const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + port;
-  console.log('Listening on ' + bind);
+app.use((req, res, next) => {
+  console.log('Request received!');
+  next();
 });
 
-server.listen(port);
+app.use((req, res, next) => {
+  res.status(201);
+  next();
+});
+
+app.use((req, res, next) => {
+  res.json({ message: 'Your request was successful!' });
+  next();
+});
+
+app.use((req, res, next) => {
+  console.log('Response sent successfully!');
+});
+
+module.exports = app;
